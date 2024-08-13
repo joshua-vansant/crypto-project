673fd8d3a4b5351e5b2e016a1c7787a19db74130
Joshua Van_Sant <joshua.vansant@outlook.com>
2024-08-13 08:00:36 -0600

    added plotly to requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///crypto_data.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://render_1_ssjk_user:wwuDljEgJ5HoCD0D5HrCfBf5YK6crvCp@dpg-cqtd6raj1k6c738j8g30-a.oregon-postgres.render.com/render_1_ssjk'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
